869bdb6124494a9316720b4e4d7b82f730e3e8e7
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-10-20 15:48:58 +0200

    preact flowbite component useState hook usage stillneeds a little fix

--- src/components/FlowbiteExample1.tsx
+++ src/components/FlowbiteExample1.tsx
@@ -1,4 +1,5 @@
 import { Modal } from 'flowbite'
+import { useState } from 'preact/hooks';
 
 
 
@@ -12,6 +13,7 @@ export interface FlowbiteExampleProps {
 }
 
 export const FlowbiteExample1 = (props: FlowbiteExampleProps) => {
+    const { isOpened, setIsOpened } = useState<boolean>(false);
     // select the two elements that we'll work with
     const $buttonElement: HTMLElement|null = document.querySelector('#pestoButton');
     const $modalElement: HTMLElement|null = document.querySelector('#pestoModal');
@@ -25,9 +27,16 @@ export const FlowbiteExample1 = (props: FlowbiteExampleProps) => {
     // modal.hide();
     return (
         <>
-            <button id="pestoButton" type="button" onClick={handleToggle} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+            <button id="pestoButton" type="button" onClick={() => {
+                                        modal.show();
+                                        //modal.hide();
+                                        setIsOpened(true)
+                                    }
+                                } class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
 
-            <div id="pestoModal" tabindex={-1} aria-hidden={true} class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            {//<div id="pestoModal" tabindex={-1} aria-hidden={true} class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            }
+            <div id="pestoModal" tabindex={-1} aria-hidden={!isOpened} class={ (isOpened?``:`hidden`) + `fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`}>
                 <div class="relative w-full max-w-2xl max-h-full">
                     {//<!-- Modal content -->
                     }
@@ -38,7 +47,12 @@ export const FlowbiteExample1 = (props: FlowbiteExampleProps) => {
                             <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
                                 Terms of Service
                             </h3>
-                            <button  onClick={handleToggle} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                            <button  onClick={() => {
+                                        // modal.show();
+                                        modal.hide();
+                                        setIsOpened(false)
+                                    }
+                                } type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </button>
                         </div>
@@ -55,8 +69,18 @@ export const FlowbiteExample1 = (props: FlowbiteExampleProps) => {
                         {//<!-- Modal footer -->
                         }
                         <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button  onClick={handleToggle} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                            <button  onClick={handleToggle} type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Decline</button>
+                            <button  onClick={() => {
+                                        // modal.show();
+                                        modal.hide();
+                                        setIsOpened(false)
+                                    }
+                            } type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                            <button  onClick={() => {
+                                        // modal.show();
+                                        modal.hide();
+                                        setIsOpened(false)
+                                    }
+                                } type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Decline</button>
                         </div>
                     </div>
                 </div>
