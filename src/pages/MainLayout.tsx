c679df6fd71e4178e49c57a13c5dd10e59f5450c
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-10-21 20:50:27 +0200

    better flowbitestarter with layout, NavBar, Footer, (Lucide)Icons

---
+++ src/pages/MainLayout.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'preact/hooks';
+import { Children } from 'preact/compat';
+import NavBar from '../components/NavBar'
+import PestoFooter from '../components/Footer';
+
+interface MainLayoutProps {
+    children: any[],
+    theme?: any,
+}
+
+export const MainLayout = ({children}: MainLayoutProps ) => {
+    const [isDark, setIsDark] = useState<boolean>(false);
+
+    return (
+        <>
+        <NavBar />
+        {//children
+        }
+        { 
+            Children.forEach(children, (child, index) => {
+                console.log(` Loop over MainLayout children, chil no.${index} : [${child}]`)
+                return child;
+            })
+        }
+
+        <PestoFooter />
+        </>
+    )
+}
+
+export default MainLayout;
